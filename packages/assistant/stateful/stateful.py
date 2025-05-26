01f0430b0e2ca6ab5061b7a26ea3dafd164d5cf3
Probu93 <robustelli.primula@gmail.com>
2025-05-26 19:07:07 +0000

    commit

--- packages/assistant/stateful/stateful.py
+++ packages/assistant/stateful/stateful.py
@@ -1,25 +1,33 @@
 import chat
+import history
 
 def stateful(args):
-  
-  inp = args.get("input", "")
-  out = f"Hello from {chat.MODEL}"
-  res = {}
-  
-  if inp != "":
-    # load the history in the chat
-    ch = chat.Chat(args)
-    #TODO:E4.2 load the history
-    #END TODO
-    # add a message and save it 
-    msg = f"user:{inp}"
-    ch.add(msg)
-    print(ch.messages)
-    out = ch.complete()
-    # complete, save the assistant and return the id
-    #TODO:E4.2 save the message and the state
-    # return the id as state field in the response
-    #END TODO
+    inp = args.get("input", "")
+    out = f"Hello from {chat.MODEL}"
+    res = {}
 
-  res['output'] = out
-  return res
+    if inp != "":
+        # Crea un'istanza della chat
+        ch = chat.Chat(args)
+
+        # Carica la cronologia precedente
+        hi = history.History(args)
+        hi.load(ch)
+
+        # Aggiungi messaggio utente
+        msg = f"user:{inp}"
+        ch.add(msg)
+
+        # Ottieni la risposta del modello
+        out = ch.complete()
+
+        # Salva lo scambio nella cronologia
+        hi.save(msg)
+        hi.save(f"assistant:{out}")
+
+        # Includi lo stato (id della cronologia) nella risposta
+        res["state"] = hi.id()
+
+    # Risultato da restituire (solo valori JSON serializzabili)
+    res["output"] = out
+    return res
